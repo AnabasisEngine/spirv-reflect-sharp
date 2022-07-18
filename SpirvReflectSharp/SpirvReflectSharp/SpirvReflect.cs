4a5c24487d4dca6cc63353941f9a1607f1230150
Khitiara <robotbrainify@gmail.com>
2022-07-18 19:03:39 -0400

    A

--- SpirvReflectSharp/SpirvReflectSharp/SpirvReflect.cs
+++ SpirvReflectSharp/SpirvReflectSharp/SpirvReflect.cs
@@ -1,29 +1,26 @@
-﻿using System;
+﻿namespace SpirvReflectSharp;
 
-namespace SpirvReflectSharp
+public unsafe class SpirvReflect
 {
-	public unsafe class SpirvReflect
-	{
-		/// <summary>
-		/// Creates a <see cref="ShaderModule"/> given SPIR-V bytecode
-		/// </summary>
-		/// <param name="shaderBytes">Compiled SPIR-V bytecode</param>
-		/// <returns>A <see cref="ShaderModule"/></returns>
-		public static ShaderModule ReflectCreateShaderModule(byte[] shaderBytes) {
-			fixed (void* shdrBytecode = &shaderBytes[0])
-			{
-				SpirvReflectNative.SpvReflectShaderModule module;
-				SpirvReflectNative.SpvReflectResult result =
-					SpirvReflectNative.spvReflectCreateShaderModule(Convert.ToUInt64(shaderBytes.LongLength), shdrBytecode, &module);
+	/// <summary>
+	/// Creates a <see cref="ShaderModule"/> given SPIR-V bytecode
+	/// </summary>
+	/// <param name="shaderBytes">Compiled SPIR-V bytecode</param>
+	/// <returns>A <see cref="ShaderModule"/></returns>
+	public static ShaderModule ReflectCreateShaderModule(ReadOnlySpan<byte> shaderBytes) {
+		fixed (void* shdrBytecode = &shaderBytes[0])
+		{
+			SpirvReflectNative.SpvReflectShaderModule module;
+			SpirvReflectNative.SpvReflectResult result =
+				SpirvReflectNative.spvReflectCreateShaderModule((ulong)shaderBytes.Length, shdrBytecode, &module);
 
-				if (result == SpirvReflectNative.SpvReflectResult.SPV_REFLECT_RESULT_SUCCESS)
-				{
-					return new ShaderModule(module);
-				}
-				else
-				{
-					throw new SpirvReflectException(result);
-				}
+			if (result == SpirvReflectNative.SpvReflectResult.SPV_REFLECT_RESULT_SUCCESS)
+			{
+				return new ShaderModule(module);
+			}
+			else
+			{
+				throw new SpirvReflectException(result);
 			}
 		}
 	}
